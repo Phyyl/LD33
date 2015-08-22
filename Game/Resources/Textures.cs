ea8104e4ad6e4dfd63c1ee100a866456134c8bd3
Richer Larivière <richerlariviere@gmail.com>
2015-08-21 23:55:11 -0400

    Added Button texture

--- Game/Resources/Textures.cs
+++ Game/Resources/Textures.cs
@@ -14,12 +14,14 @@ namespace Game.Resources
         public static Texture Passive1 { get; private set; }
         public static Texture Monster1 { get; private set; }
         public static Texture Ground { get; private set; }
+        public static Texture Button { get; private set; }
 
         public static void Load()
         {
             Passive1 = LoadTexture("passive1.png");
             Monster1 = LoadTexture("monster1.png");
             Ground =   LoadTexture("ground.png");
+            Button =   LoadTexture("button.png");
         }
 
         private static Texture LoadTexture(string fileName)
