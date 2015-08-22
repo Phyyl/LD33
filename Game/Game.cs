9362b296614ee4b830f6030d3708f03b723b077e
Philippe Paré <Phyyl@live.ca>
2015-08-22 00:44:19 -0400

    Added basic game class logic

--- Game/Game.cs
+++ Game/Game.cs
@@ -1,4 +1,7 @@
 ﻿using Game.Resources;
+using Graphics;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +12,17 @@ namespace Game
 {
     public partial class Game
     {
+        public const float SCALE = 2;
+
+        private World world;
+
         public void Load()
         {
             Textures.Load();
             SpriteSheets.Load();
             Animations.Load();
 
+            world = new World();
         }
 
         public void Resize(int width, int height)
@@ -24,12 +32,17 @@ namespace Game
 
         public void Update(float delta)
         {
-
+            world.Update(delta);
         }
 
         public void Render(float delta)
         {
-
+            GL.PushMatrix();
+            {
+                GL.Scale(SCALE, SCALE, 1);
+                world.Render(delta);
+            }
+            GL.PopMatrix();
         }
     }
 }
